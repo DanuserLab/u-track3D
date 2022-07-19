b29c591cadf79f0d14b7622207f15fafc344813e
Danuser CI User <danuser_ci@NucleusA251.cm.cluster>
2022-07-19 11:04:11 -0500

    Danuser Lab CI pipeline build # 158272

--- software/retrieveRemoteDataset.m
+++ software/retrieveRemoteDataset.m
@@ -1,19 +1,17 @@
-function copyright = getLCCBCopyright()
-%
-% This is a user-defined function used in UTSW software. 
-% It is called when any GUI is generated. It configures the copyright
-% information.
-%
-% Input: 
-%
-%
-% Output:
-%
-%   copyright - String: copyright and version information
-%
-% Chuangang Ren, 11/2010
-% Sebastien Besson, Feb 2013
-% Andrew Jamieson, Nov 2016 - UTSW
+function [dataFolder]=retrieveRemoteDataset(datasetUrl,outputFolder)
+mkdir(outputFolder)
+[~,datasetName,fileExt]=fileparts(datasetUrl);
+zipOutputFilename=fullfile(outputFolder,[datasetName,fileExt]);
+dataFolder=fullfile(outputFolder,datasetName); 
+
+disp('::::')
+disp(['Downloading and decompressing dataset ' datasetName ' under:']);
+disp(dataFolder)                
+
+websave(zipOutputFilename, datasetUrl);
+unzip(zipOutputFilename,[outputFolder])  
+
+
 %
 % Copyright (C) 2022, Danuser Lab - UTSouthwestern 
 %
@@ -33,8 +31,3 @@ function copyright = getLCCBCopyright()
 % along with NewUtrack3DPackage.  If not, see <http://www.gnu.org/licenses/>.
 % 
 % 
-
-% Set year and version information
-str_year = datestr(date,'YYYY');
-copyright = sprintf('Copyright %s Danuser Lab - UTSouthwestern', str_year);
-% -- TEST CI pipeline deploy to GITHUB -- CI pipeline build # 158272
